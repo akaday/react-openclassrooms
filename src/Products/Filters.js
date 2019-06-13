56f6c876bcdfa6e9185f659459cc0bc72ad4519d
akaday <cakra.ds@gmail.com>
2019-06-13 14:51:11 +0700

    bismillah

---
+++ src/Products/Filters.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Filters extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          &nbsp;Only show stocked products
+        </p>
+      </form>
+    );
+  }
+}
+
+export default Filters;
